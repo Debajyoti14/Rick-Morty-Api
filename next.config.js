9469416541292280ef322083a592dc4e43c350d8
Debajyoti <debajyotisaha14@gmail.com>
2022-02-18 17:06:58 +0530

    Character specific page created

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  Images:{
+    domain : ["https://rickandmortyapi.com/"],
+    loaders:"custom",
+    path:"/"
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/characters/[id].tsx
@@ -0,0 +1,31 @@
+import { Character,GetCharacterResult } from "../../types";
+import Image from 'next/image'
+import imageLoader from "../../imageLoader";
+
+function characterPage({character}:{character: Character}) {
+  return <div>
+                <h1>{character.name}</h1>
+          <Image loader={imageLoader} unoptimized src={character.image} alt={character.name} width="200px" height="200px"/>
+  </div>;
+}
+
+export async function getStaticPaths() {
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const { results }: GetCharacterResult = await res.json();
+  return {
+    paths: results.map((character) => {
+      return { params: { id: String(character.id) } };
+    }),
+  };
+}
+
+export async function getStaticProps({params} : {params: {id: String}}){
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
+    const character = res.json();
+    return{
+        props:{
+            character
+        }
+    }
+ 
+}
